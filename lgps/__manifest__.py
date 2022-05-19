410176280e80e24172b524aa4a211e8a6f7cb890
javina-lmbl <jorge.avina@logicamobile.com>
2022-05-19 15:27:43 -0500

    first commit

---
+++ lgps/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'lgps',
+    'description': 'Intralix module for internal processes',
+    'author': 'Intralix',
+    'application': True,
+    'website': 'https://www.intralix.com',
+    'category': 'Uncategorized',
+    'version': '15.0.1.0.0',
+    'depends': [
+        'base',
+        'stock',
+        'contacts',
+        'account',        
+        'crm',
+        'sale_subscription',
+        'helpdesk',
+        'mail',
+        'project',
+        'sale'
+    ],
+    'data': [
+    ],
+}
